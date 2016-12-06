eef7beec35b51c53868ded36e85c74bb06b35099
tlycken <tomas.lycken@gmail.com>
2016-12-06 08:14:08 +0100

    Solve 6 a+b

---
+++ 2016/6/runner6.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open AoC.Utils.Runner
+open AoC.Dec6.Solver
+
+[<EntryPoint>]
+let main argv = 
+
+    let solution = solve argv A.solve B.solve
+
+    printfn "%s" solution
+
+    0 // return an integer exit code
---
+++ 2016/6/solution6.fs
@@ -0,0 +1,46 @@
+namespace AoC.Dec6
+
+module Domain =
+
+    open AoC.Utils.Helpers
+
+    let classify str =
+        str
+        |> asChars
+        |> List.mapi (fun i c -> i,c)
+
+    let pos = fst
+    let char = snd
+
+    let mostPopular = (List.groupBy id >> List.maxBy (snd >> List.length) >> fst)
+
+    let leastPopular = (List.groupBy id >> List.minBy (snd >> List.length) >> fst)
+
+    let chars = snd >> List.map char
+
+module Solver =
+
+    module A =
+
+        open AoC.Utils.Helpers
+        open Domain
+
+        let solve input =
+
+            input
+            |> List.collect classify
+            |> List.groupBy pos
+            |> List.map (chars >> mostPopular)
+            |> join
+
+    module B =
+
+        open AoC.Utils.Helpers
+        open Domain
+
+        let solve input =
+            input
+            |> List.collect classify
+            |> List.groupBy pos
+            |> List.map (chars >> leastPopular)
+            |> join
