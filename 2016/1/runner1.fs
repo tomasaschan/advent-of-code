9587e23cb45d2548582c856a81dafe500c5f62a5
tlycken <tomas.lycken@gmail.com>
2016-12-07 07:41:11 +0100

    Rewrite 1 with a solution to 1b

--- 2016/1/runner1.fs
+++ 2016/1/runner1.fs
@@ -1,13 +1,12 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open AoC.Utils.Runner
-open Dec1.Solutions.A
-open Dec1.Solutions.B
+open AoC.Dec1.Solutions
 
 [<EntryPoint>]
 let main argv =
 
-    let solutions = solve1 argv solveA solveB
+    let solutions = solve1 argv A.solve B.solve
     
     printfn "%s" solutions
     0 // return an integer exit code
---
+++ 2016/1/solution1.fs
@@ -0,0 +1,96 @@
+namespace AoC.Dec1
+
+module Domain =
+
+    open AoC.Utils.Types
+
+    type State = {
+        here : Position
+        facing : Direction
+        prev : State option
+    }
+    type Instruction = {
+        turn : Turn
+        steps : int
+    }
+
+    let turn d s = { s with facing = turn s.facing d }
+    
+    let move1 s = { s with here = move1 s.facing s.here; prev = Some s }
+
+    let distance s = abs(s.here.x) + abs(s.here.y)
+
+    let rec crosses pos s =
+        match s with
+        | None -> false
+        | Some state -> state.here = pos || crosses pos state.prev
+
+    module A =
+    
+        let rec move i s =
+            match i with
+            | 0 -> s
+            | _ -> move (i-1) (move1 s)
+
+        let follow s i = (turn i.turn >> move i.steps) s
+
+        let followFrom = Seq.fold follow
+
+    module B =
+        let isCrossing s = crosses s.here s.prev
+
+        let stopIfCrossing s next =
+            if isCrossing s
+            then s
+            else next s
+
+        let rec move i s =
+            match i with
+            | 0 -> s
+            | _ -> stopIfCrossing (move1 s) (move (i-1))
+
+        let follow s i = (turn i.turn >> move i.steps) s
+
+        let rec followFrom state instrs =
+            match instrs with
+            | [] -> state
+            | i::is -> stopIfCrossing (follow state i) (fun s -> followFrom s is)
+
+module Parsing =
+
+    open AoC.Utils.Types
+    open AoC.Utils.Helpers
+    open Domain
+    
+    let stepcountFromChars = join >> parseInt
+
+    let instructionFromString input =
+        match asChars input with
+        | 'R'::digits -> (Some Right, stepcountFromChars digits)
+        | 'L'::digits -> (Some Left, stepcountFromChars digits)
+        | _ -> (None, None)
+
+    let parse data =
+        match instructionFromString data with
+        | (Some t, Some s) -> Some { turn = t; steps = s }
+        | _ -> None
+
+    let instructions = split ", " >> List.choose parse
+
+module Solutions =
+
+    open Domain
+    open Parsing
+    open AoC.Utils.Types
+
+    let initState = { here = { x = 0; y = 0 }; facing = North; prev = None }
+
+    module A =
+
+        open Domain.A
+        let solve = instructions >> followFrom initState >> distance >> sprintf "%d"
+
+    module B =
+
+        open Domain.B
+        let solve = instructions >> followFrom initState >> distance >> sprintf "%d"
