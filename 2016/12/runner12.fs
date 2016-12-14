e5b10b60df135b120a4a857ea544e966a9eba882
tlycken <tomas.lycken@gmail.com>
2016-12-14 07:39:55 +0100

    Incorrect solution to 12 a

---
+++ 2016/12/runner12.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open AoC.Utils.Runner
+open AoC.Dec12.Solver
+
+[<EntryPoint>]
+let main argv =
+
+    solve argv A.solve todo |> printfn "%s"
+
+    0 // return an integer exit code
