2c4465dbf2353cdbe8e1713af14f5e44e3b946c2
Tomas Lycken <tomas.lycken@wasakredit.se>
2016-12-19 17:26:41 +0100

    Start working on 19 dec

---
+++ 2016/19/solution19.fs
@@ -0,0 +1,29 @@
+namespace AoC.Dec19
+
+module Domain =
+
+    open AoC.Utils.Helpers
+
+    let helpers count = Seq.init count id
+
+    let second<'a> = Seq.skip 1 >> Seq.tryHead
+
+    let third<'a> = Seq.skip 2 >> Seq.tryHead
+    let exceptSecond s =
+        match Seq.tryHead s with
+        | Some h -> (Seq.skipOrEmpty 2 >> Seq.prepend h) s
+        | None -> s
+
+    let rec play helpers =
+        match (Seq.skip 1 >> Seq.tryHead) helpers with
+        | Some h -> play (Seq.append [h] (Seq.skip 2 helpers))
+        | _ -> None
+
+
+module Solver =
+
+    open Domain
+
+    module A =
+
+        let solve = helpers >> play
