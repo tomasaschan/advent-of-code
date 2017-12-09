82d9374891a4f7cf3ccdcfd5e0a8d2e1acfbcdf0
Tomas Lycken <tlycken@users.noreply.github.com>
2017-12-09 22:55:50 +0100

    Solve 7b

--- 2017/solver/Dec7.fs
+++ 2017/solver/Dec7.fs
@@ -22,38 +22,65 @@ module Parse =
 
   let lines = List.fold (fun (weights, discs) l -> line weights discs l) (Map.empty, Map.empty)
 
-let root weights discs =
-  let carriers = weights |> Map.toList |> List.map fst |> Set.ofList
-  let carriees = discs |> Map.toList |> List.collect snd |> Set.ofList
+let carrier discs name =
+  let keyFinder _ ps = ps |> List.contains name
+  Map.tryFindKey keyFinder discs
 
-  Set.difference carriers carriees |> Set.maxElement
+
+let rec _root discs candidate =
+  match carrier discs candidate with
+  | Some c -> _root discs c
+  | None -> candidate
+
+let root discs =
+  let firstCandidate = discs |> Map.toList |> List.head |> fst
+  _root discs firstCandidate
+
+let onTopOf discs name = Map.tryFind name discs |> orDefault []
 
 module A =
 
-  let solve = Parse.lines >> (fun (ws,ds) -> root ws ds)
+  let solve = Parse.lines >> snd >> root
 
 module B =
 
-  let balanced towers = List.distinct towers |> List.length = 1
+  let balanced : int list -> bool = List.distinct >> List.length >> (=) 1
 
-  let rec calculate weights discs name =
-    let self = Map.find weights name
+  let minmax lst = List.min lst, List.max lst
 
-    match Map.tryFind name discs with
-    | Some towers ->
-      let above = towers |> List.map (calculate weights discs)
-      if not << 
-      self, List.sum above, above |> List.map snd |> balanced
-    | None -> self, self, true
-
-  // let rec aboveSum weights discs name =
-    
+  let outlier = List.groupBy id >> List.sortBy (snd >> List.length) >> List.head >> snd >> List.head
 
   let solve input =
+    let weights, discs = Parse.lines input
 
-    // let total = identifyMismatch weights discs 0 root
+    let rec totalWeight name =
+      let self = Map.find name weights
+      let above = onTopOf discs name |> List.sumBy totalWeight
+      self + above
 
-    // sprintf "%i" total
-    "TODO"
+    let rec findImbalancedTower candidate =
+      let above = Map.find candidate discs
+      let was = above |> List.map totalWeight
+
+      if balanced was
+      then candidate
+      else
+        let i = List.findIndex (fun w -> w = outlier was) was
+        let candidate' = above.[i]
+        findImbalancedTower candidate'
+
+    let root = root discs
+    let imbalanced = findImbalancedTower root
+    let parent = match carrier discs imbalanced with Some p -> p | None -> failwith "fuck"
+    let above = Map.find parent discs |> List.map totalWeight
+
+    let strange = outlier above
+
+    let i = List.findIndex ((=) strange) above
+    let outlier = above.[i]
+    let j = List.findIndex ((<>) strange) above
+    let normal = above.[j]
+    let im = Map.find imbalanced weights
+    im + normal - outlier |> sprintf "%i"
 
 let solvers = A.solve, B.solve
