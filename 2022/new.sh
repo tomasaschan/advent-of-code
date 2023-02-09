a0e1d5c228a6e1062b96857abfd710bd1e5a1e3f
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2023-02-09 21:06:26 +0100

    Make new solver files better

--- 2022/new.sh
+++ 2022/new.sh
@@ -3,7 +3,26 @@
 day=${1?"usage: $0 <day>"}
 
 mkdir -p src/dec$day
-echo "#!/usr/bin/env python3" > src/dec$day/solve.py
+cat > src/dec$day/solve.py <<EOF
+#!/usr/bin/env python3
+
+import sys
+
+input = sys.stdin.read()
+
+
+def a():
+    pass
+
+
+def b():
+    pass
+
+
+print("a:", a())
+print("b:", b())
+EOF
+
 chmod +x src/dec$day/solve.py
 touch src/dec$day/input.txt
 touch src/dec$day/example.txt
