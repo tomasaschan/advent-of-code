311900ca67f12ad4e0fb5caa3dd1f19d62f57651
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2023-12-11 21:34:47 +0100

    Dec 09 ⭐⭐

--- 2023/aoc/lib/dec09.ex
+++ 2023/aoc/lib/dec09.ex
@@ -9,15 +9,27 @@ defmodule Dec09 do
     |> Enum.map(fn line -> line |> String.split(" ") |> Enum.map(&String.to_integer/1) end)
   end
 
-  def extrapolate(history) do
+  def extrapolate(history, {take, combine}) do
     diffs = history |> Enum.zip(history |> Enum.drop(1)) |> Enum.map(fn {a, b} -> b - a end)
 
-    last = history |> List.last()
+    extreme = take.(history)
 
     cond do
-      Enum.all?(diffs, fn n -> n == 0 end) -> last
-      true -> last + extrapolate(diffs)
+      Enum.all?(diffs, fn n -> n == 0 end) -> extreme
+      true -> combine.(extreme, extrapolate(diffs, {take, combine}))
     end
+  end
+
+  @doc """
+  iex> Dec09.future([0, 3, 6, 9, 12, 15])
+  18
+  iex> Dec09.future([1, 3, 6, 10, 15, 21])
+  28
+  iex> Dec09.future([10, 13, 16, 21, 30, 45])
+  68
+  """
+  def future(history) do
+    extrapolate(history, {&List.last/1, &(&1 + &2)})
   end
 
   @doc """
@@ -28,7 +40,31 @@ defmodule Dec09 do
     histories = parse(input)
 
     histories
-    |> Enum.map(&extrapolate/1)
+    |> Enum.map(&future/1)
+    |> Enum.sum()
+  end
+
+  @doc """
+  iex> Dec09.past([0, 3, 6, 9, 12, 15])
+  -3
+  iex> Dec09.past([1, 3, 6, 10, 15, 21])
+  0
+  iex> Dec09.past([10, 13, 16, 21, 30, 45])
+  5
+  """
+  def past(history) do
+    extrapolate(history, {&List.first/1, &(&1 - &2)})
+  end
+
+  @doc """
+  iex> Dec09.b("0 3 6 9 12 15\\n1 3 6 10 15 21\\n10 13 16 21 30 45\\n")
+  2
+  """
+  def b(input) do
+    histories = parse(input)
+
+    histories
+    |> Enum.map(&past/1)
     |> Enum.sum()
   end
 end
--- 2023/aoc/test/dec09_test.exs
+++ 2023/aoc/test/dec09_test.exs
@@ -5,6 +5,6 @@ defmodule Dec09Test do
   test "Dec 09" do
     input = File.read!("../../inputs/2023/09.txt")
     assert Dec09.a(input) == 2_038_472_161
-    # assert Dec09.b(input) == 0
+    assert Dec09.b(input) == 1091
   end
 end
