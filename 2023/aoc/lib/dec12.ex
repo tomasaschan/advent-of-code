8501b5dd85ea80c59a910fbf02a18e2f1da43530
Tomas Aschan <tomasl@spotify.com>
2023-12-22 12:42:57 +0100

    Dec 12 ⭐⭐❓ (sloooow...)

--- 2023/aoc/lib/dec12.ex
+++ 2023/aoc/lib/dec12.ex
@@ -141,9 +141,26 @@ defmodule Dec12 do
   iex> Dec12.n_valid("?###????????", [3, 2, 1])
   10
   """
-  def n_valid(conditions, requirements) do
-    expand(conditions) |> Enum.filter(fn conds -> valid?(conds, requirements) end) |> Enum.count()
-  end
+  def n_valid("", []), do: 1
+  def n_valid("." <> springs, spec), do: n_valid(springs, spec)
+  def n_valid("#" <> _, []), do: 0
+  def n_valid("", [_ | _]), do: 0
+  def n_valid("#" <> springs, [d | spec]), do: n_valid_group(springs, d - 1, spec)
+
+  def n_valid("?" <> springs, spec),
+    do: n_valid("#" <> springs, spec) + n_valid("." <> springs, spec)
+
+  def n_valid_group("#" <> _, 0, _), do: 0
+  def n_valid_group("#" <> springs, d, spec) when d > 0, do: n_valid_group(springs, d - 1, spec)
+  def n_valid_group("." <> springs, 0, spec), do: n_valid(springs, spec)
+  def n_valid_group("." <> _, d, _) when d > 0, do: 0
+  def n_valid_group("", 0, spec), do: n_valid("", spec)
+  def n_valid_group("", d, _) when d > 0, do: 0
+
+  def n_valid_group("?" <> springs, d, spec) when d > 0,
+    do: n_valid_group("#" <> springs, d, spec)
+
+  def n_valid_group("?" <> springs, 0, spec), do: n_valid("." <> springs, spec)
 
   @doc """
   iex> Dec12.a("???.### 1,1,3
@@ -157,5 +174,28 @@ defmodule Dec12 do
   """
   def a(input) do
     input |> parse() |> Enum.map(fn {conds, reqs} -> n_valid(conds, reqs) end) |> Enum.sum()
+  end
+
+  @doc """
+  iex> Dec12.b("???.### 1,1,3
+  ...>.??..??...?##. 1,1,3
+  ...>?#?#?#?#?#?#?#? 1,3,1,6
+  ...>????.#...#... 4,1,1
+  ...>????.######..#####. 1,6,5
+  ...>?###???????? 3,2,1
+  ...>")
+  525152
+  """
+  def b(input) do
+    input
+    |> parse()
+    |> Enum.map(fn {springs, spec} ->
+      {
+        [springs, springs, springs, springs, springs] |> Enum.join("?"),
+        Enum.concat([spec, spec, spec, spec, spec])
+      }
+    end)
+    |> Enum.map(fn {springs, spec} -> n_valid(springs, spec) end)
+    |> Enum.sum()
   end
 end
--- 2023/aoc/test/dec12_test.exs
+++ 2023/aoc/test/dec12_test.exs
@@ -2,9 +2,11 @@ defmodule Dec12Test do
   use ExUnit.Case
   doctest Dec12
 
+  @tag timeout: :infinity
   test "Dec 12" do
     input = File.read!("../../inputs/2023/12.txt")
 
     assert Dec12.a(input) == 7350
+    assert Dec12.b(input) == 0
   end
 end
