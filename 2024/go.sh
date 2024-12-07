f353cda6ec5a8614b8fe7d606a5b09a5cb020b6c
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2024-12-07 01:18:25 +0100

    Only run a single target with go.sh

--- 2024/go.sh
+++ 2024/go.sh
@@ -3,4 +3,4 @@
 d2=$(printf "%02d" $1)
 name=dec$d2
 
-stack build --file-watch --test --exec "bash -c \"stack exec $name-run < ../inputs/2024/$d2.txt\""
+stack build $name --file-watch --test --exec "bash -c \"stack exec $name-run < ../inputs/2024/$d2.txt\""
