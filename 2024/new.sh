643f62de571cd14b1a6819a57c6826ef2427f5f2
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2024-12-09 22:03:46 +0100

    Improve templating more

--- 2024/new.sh
+++ 2024/new.sh
@@ -7,7 +7,8 @@ stack new $name ./template/template.hsfiles
 
 sed -i "s/$name/Dec $1/" $name/test/LibSpec.hs
 find $name -name '*.hs' -not -path '*.stack-work*' -print0 | xargs -0 sed -i "s/Lib/Dec$d2/g"
-find $name -name 'Lib*.hs'| xargs rename Lib Dec$d2
+find $name -name 'Lib*.hs' | xargs rename Lib Dec$d2
+sed -i "s/00.txt/$d2.txt/" $name/test/Dec$d2Spec.hs
 
 yq -i '.packages += ["'$name'"]' stack.yaml
 rm $name/stack.yaml
