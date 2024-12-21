10c83e2edfc560bcbd6f8562674261342dd840d9
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2024-12-21 14:06:58 +0100

    Fixup init script

--- 2024/new.sh
+++ 2024/new.sh
@@ -3,7 +3,7 @@
 d2=$(printf "%02d" $1)
 name=dec$d2
 
-stack new $name ./template/template.hsfiles
+stack new --no-init $name ./template/template.hsfiles
 
 sed -i "s/$name/Dec $1/" $name/test/LibSpec.hs
 find $name -name '*.hs' -not -path '*.stack-work*' -print0 | xargs -0 sed -i "s/Lib/Dec$d2/g"
@@ -11,4 +11,3 @@ find $name -name 'Lib*.hs' | xargs rename Lib Dec$d2
 sed -i "s/00.txt/$d2.txt/" $name/test/Dec${d2}Spec.hs
 
 yq -i '.packages += ["'$name'"]' stack.yaml
-rm $name/stack.yaml
