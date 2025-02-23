55c643c6cb977d5c74c1bb0059694be77a581527
Tomas Aschan <tomasl@spotify.com>
2025-02-23 14:17:13 +0100

    Dec 10 ⭐⭐

--- 2024/dec10/src/Dec10.hs
+++ 2024/dec10/src/Dec10.hs
@@ -14,7 +14,7 @@ import Data.Sequence
     singleton,
     (><),
   )
-import Data.Set (Set, empty, insert, notMember)
+import qualified Data.Set as Set
 import Data.Tuple.Extra (dupe)
 import Helpers.Data.Grid (Sparse, bounds, neighbors, sparse, (!), (!?))
 import Text.Read (readMaybe)
@@ -22,13 +22,20 @@ import Text.Read (readMaybe)
 solve :: String -> (String, String)
 solve = bimap a b . dupe
   where
-    a input = show . sum . fmap (hike g) . trailheads $ g
-      where
-        g = interpret input
-    b = const ""
+    a = show . score . hikeAll . interpret
+    b = show . rating . hikeAll . interpret
 
 interpret :: String -> Sparse Int
 interpret = sparse (readMaybe . Data.List.singleton)
+
+score :: [[(Int, Int)]] -> Int
+score = sum . fmap (length . Set.fromList)
+
+rating :: [[(Int, Int)]] -> Int
+rating = sum . fmap length
+
+hikeAll :: Sparse Int -> [[(Int, Int)]]
+hikeAll g = fmap (hike g) . trailheads $ g
 
 trailheads :: Sparse Int -> [(Int, Int)]
 trailheads g = [(x, y) | x <- [xlo .. xhi], y <- [ylo .. yhi], g ! (x, y) == 0]
@@ -44,15 +51,14 @@ nextFrom g c = mapMaybe f $ neighbors c
         then Just c'
         else Nothing
 
-hike :: Sparse Int -> (Int, Int) -> Int
-hike g c = length $ bfs g empty empty (Data.Sequence.singleton c)
+hike :: Sparse Int -> (Int, Int) -> [(Int, Int)]
+hike g c = bfs g [] (Data.Sequence.singleton c)
 
-bfs :: Sparse Int -> Set (Int, Int) -> Set (Int, Int) -> Seq (Int, Int) -> Set (Int, Int)
-bfs _ peaks _ Empty = peaks
-bfs g peaks visited (c :<| q) = bfs g peaks' visited' q'
+bfs :: Sparse Int -> [(Int, Int)] -> Seq (Int, Int) -> [(Int, Int)]
+bfs _ peaks Empty = peaks
+bfs g peaks (c :<| q) = bfs g peaks' q'
   where
     h = g ! c
-    peaks' = if h == 9 then insert c peaks else peaks
-    visited' = insert c visited
-    q'' = fromList $ filter (`notMember` visited) $ nextFrom g c
+    peaks' = if h == 9 then c : peaks else peaks
+    q'' = fromList $ nextFrom g c
     q' = q >< q''
--- 2024/dec10/test/Dec10Spec.hs
+++ 2024/dec10/test/Dec10Spec.hs
@@ -20,15 +20,9 @@ spec = describe "Dec 10" $ do
   it "should solve a" $ do
     fst solution `shouldBe` "36"
 
-  it "should be correct about the trailhead starting at (2,5)" $ do
-    let g = interpret input
-    let peaks = hike g (2, 5)
-
-    peaks `shouldBe` 1
-
   it "should solve b" $ do
     snd solution `shouldBe` "81"
 
---   it "should solve real input" $ do
---     realInput <- readFile "../../inputs/2024/10.txt"
---     solve realInput `shouldBe` ("0", "0")
+  it "should solve real input" $ do
+    realInput <- readFile "../../inputs/2024/10.txt"
+    solve realInput `shouldBe` ("825", "1805")
