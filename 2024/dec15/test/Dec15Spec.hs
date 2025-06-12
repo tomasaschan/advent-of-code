31a2bd957afc1b984c37d4eea3707f8f9bf6557e
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2025-06-12 23:29:14 +0200

    Dec 15 ⭐⭐
    Improve test helper

--- 2024/dec15/test/Dec15Spec.hs
+++ 2024/dec15/test/Dec15Spec.hs
@@ -3,25 +3,25 @@ module Dec15Spec (spec) where
 import Dec15
 import Test.Hspec
 
-withInput :: String -> SpecWith (String, String) -> Spec
-withInput file = before $ do
-  input <- readFile file
-  return $ solve input
+describeInput :: String -> String -> SpecWith (String, String) -> Spec
+describeInput description file =
+  describe description
+    . before (solve <$> readFile ("../../inputs/2024/" <> file))
 
 spec :: Spec
 spec = describe "Dec 15" $ do
-  describe "small example" $ withInput "../../inputs/2024/15-smaller.txt" $ do
+  describeInput "small example" "15-smaller.txt" $ do
     it "should solve a" $ \solution -> do
       fst solution `shouldBe` "2028"
 
-  describe "larger example" $ withInput "../../inputs/2024/15-larger.txt" $ do
+  describeInput "larger example" "15-larger.txt" $ do
     it "should solve a" $ \solution -> do
       fst solution `shouldBe` "10092"
 
     it "should solve b" $ \solution -> do
       snd solution `shouldBe` "9021"
 
-  describe "real input" $ withInput "../../inputs/2024/15.txt" $ do
+  describeInput "real input" "15.txt" $ do
     it "should solve a" $ \solution -> do
       fst solution `shouldBe` "1514333"
 
