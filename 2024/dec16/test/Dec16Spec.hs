b1451b3fc640bf135c1b211ad4323a0edf5840f9
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2025-07-01 11:26:36 +0200

    Dec 16 ⭐⭐
    Use new test helper

--- 2024/dec16/test/Dec16Spec.hs
+++ 2024/dec16/test/Dec16Spec.hs
@@ -2,29 +2,25 @@ module Dec16Spec (spec) where
 
 import Dec16
 import Test.Hspec
-
-describeInput :: String -> String -> SpecWith (String, String) -> Spec
-describeInput description file =
-  describe description
-    . before (solve <$> readFile ("../../inputs/2024/" <> file))
+import Test.Helpers
 
 spec :: Spec
 spec = describe "Dec 16" $ do
-  describeInput "small example" "16-small.txt" $ do
+  describeInput "small example" "16-small.txt" solve $ do
     it "should solve a" $ \(a, _) -> do
       a `shouldBe` "7036"
 
     it "should solve b" $ \(_, b) -> do
       b `shouldBe` "45"
 
-  describeInput "large example" "16-large.txt" $ do
+  describeInput "large example" "16-large.txt" solve $ do
     it "should solve a" $ \(a, _) -> do
       a `shouldBe` "11048"
 
     it "should solve b" $ \(_, b) -> do
       b `shouldBe` "64"
 
-  describeInput "real input" "16.txt" $ do
+  describeInput "real input" "16.txt" solve $ do
     it "should solve a" $ \(a, _) -> do
       a `shouldBe` "127520"
 
