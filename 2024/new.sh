f014687507b93c753d270f0720434cf8d90a2e1c
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2025-07-01 11:26:59 +0200

    Use new test helper in scaffold

--- 2024/new.sh
+++ 2024/new.sh
@@ -8,6 +8,6 @@ stack new --no-init $name ./template/template.hsfiles
 sed -i "s/$name/Dec $1/" $name/test/LibSpec.hs
 find $name -name '*.hs' -not -path '*.stack-work*' -print0 | xargs -0 sed -i "s/Lib/Dec$d2/g"
 find $name -name 'Lib*.hs' | xargs rename Lib Dec$d2
-sed -i "s/00.txt/$d2.txt/" $name/test/Dec${d2}Spec.hs
+sed -i "s/00\(.*\).txt/$d2\1.txt/" $name/test/Dec${d2}Spec.hs
 
 yq -i '.packages += ["'$name'"]' stack.yaml
