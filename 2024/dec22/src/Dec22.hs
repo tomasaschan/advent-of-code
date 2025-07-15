02c399177ecc3fcb69485d988d9dd50979bc4e18
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2025-07-15 14:39:00 +0200

    Dec 22 ⭐⭐
    Sligthly slower, but more elegant?

--- 2024/dec22/src/Dec22.hs
+++ 2024/dec22/src/Dec22.hs
@@ -7,14 +7,15 @@ module Dec22
 where
 
 import Data.Bits (xor)
-import Data.Map (Map, alter, elems, unionsWith)
+import Data.Map (Map, alter, elems)
+import Data.Set (Set, insert, member)
 
 solve :: String -> (String, String)
 solve input = (a initials, b initials)
   where
     initials = parse input
     a = show . sum . fmap ((!! 2000) . iterate evolve)
-    b = show . maximum . elems . unionsWith (+) . fmap pricesByChanges
+    b = show . maximum . elems . foldl (flip pricesByChanges) mempty
 
 parse :: String -> [Int]
 parse = fmap read . lines
@@ -36,7 +37,7 @@ type Key = Int
 key :: (Int, Int, Int, Int) -> Key
 key (a, b, c, d) = ((19 :: Int) ^ (3 :: Int)) * (a + 9) + ((19 :: Int) ^ (2 :: Int)) * (b + 9) + 19 * (c + 9) + (d + 9)
 
-pricesByChanges :: Int -> Map Key Int
+pricesByChanges :: Int -> Map Key Int -> Map Key Int
 pricesByChanges start = build'''' 2000 start mempty
   where
     build'''' k n = build''' (k - 1) (evolve n) (price (evolve n) - price n)
@@ -44,16 +45,20 @@ pricesByChanges start = build'''' 2000 start mempty
     build'' k n = build' (k - 1) (evolve n) (price (evolve n) - price n)
     build' k n = build (k - 1) (evolve n) (price (evolve n) - price n)
 
-    build :: Int -> Int -> Int -> Int -> Int -> Int -> Map Key Int -> Map Key Int
-    build 0 _ _ _ _ _ acc = acc
-    build k n d c b a acc =
-      build (k - 1) n' d' d c b acc'
+    build :: Int -> Int -> Int -> Int -> Int -> Int -> Set Key -> Map Key Int -> Map Key Int
+    build 0 _ _ _ _ _ _ acc = acc
+    build k n d c b a seen acc
+      | k' `member` seen = build (k - 1) n' d' d c b seen acc
+      | otherwise = build (k - 1) n' d' d c b seen' acc'
       where
         n' = evolve n
         p = price n
         p' = price n'
         d' = p' - p
         k' = key (a, b, c, d)
+
+        seen' = insert k' seen
         acc' = alter f k' acc
 
-        f = maybe (Just p) Just
+        f Nothing = Just p
+        f (Just p'') = Just (p + p'')
